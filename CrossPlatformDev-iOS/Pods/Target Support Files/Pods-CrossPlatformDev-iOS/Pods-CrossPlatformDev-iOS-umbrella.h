e1a077000de8276d2098be48d1cd7a5d9564061c
Zachary Gover <zachary1748@gmail.com>
2017-01-09 18:29:24 -0600

    Setup new project with Firebase added.

---
+++ CrossPlatformDev-iOS/Pods/Target Support Files/Pods-CrossPlatformDev-iOS/Pods-CrossPlatformDev-iOS-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CrossPlatformDev_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CrossPlatformDev_iOSVersionString[];
+
